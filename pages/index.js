afa2d45d46a5987abe1361b23b0e39ec50e8b4f7
GloriaBargoria <bargoriagloria@gmail.com>
2022-03-30 14:14:10 +0300

    categories creates, image url configuration and homepage updates

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import CategoryCard from "../components/CatergoryCard";
+import styles from '../styles/Home.module.css';
+
 const HomePage = () => {
   return (
-    <main>
-      <h1>Shopping Cart</h1>
+    <main className={styles.container}>
+      <div className={styles.small}>
+        <CategoryCard image="https://i.ibb.co/hRkHhXT/game-controller-g297cc6479-640.jpg" name="Xbox" />
+        <CategoryCard image="https://i.ibb.co/hRkHhXT/game-controller-g297cc6479-640.jpg" name="PS5" />
+        <CategoryCard image="https://i.ibb.co/hRkHhXT/game-controller-g297cc6479-640.jpg" name="Switch" />
+      </div>
+      <div className={styles.large}>
+        <CategoryCard image="https://i.ibb.co/hRkHhXT/game-controller-g297cc6479-640.jpg" name="PC" />
+        <CategoryCard
+          image="https://i.ibb.co/hRkHhXT/game-controller-g297cc6479-640.jpg1"
+          name="Accessories"
+        />
+      </div>
     </main>
   );
 };
