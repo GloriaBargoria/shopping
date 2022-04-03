3465119999f563e3a8abb84f43dac9bd8f215765
GloriaBargoria <bargoriagloria@gmail.com>
2022-04-04 00:10:36 +0300

    packages updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer'
+import store from '../redux/store';
+import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }) {
   return(
-    <div className="wrapper">
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className="wrapper">
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </Provider>
   )
 }
 
